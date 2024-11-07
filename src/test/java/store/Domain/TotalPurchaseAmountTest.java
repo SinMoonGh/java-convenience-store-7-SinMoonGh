49b34c8372ae32c47ce915a63f1f7f2c405e6a86
SinMoonGh <109773691+SinMoonGh@users.noreply.github.com>
2024-11-07 23:16:02 +0900

    test(TotalPurchaseAmountTest): 상품이 프로모션 혜택을 받는 지 확인

--- src/test/java/store/Domain/TotalPurchaseAmountTest.java
+++ src/test/java/store/Domain/TotalPurchaseAmountTest.java
@@ -3,6 +3,8 @@ package store.Domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class TotalPurchaseAmountTest {
     @Test
@@ -20,6 +22,20 @@ public class TotalPurchaseAmountTest {
         int quantity = 8;
         TotalPurchaseAmount totalPurchaseAmount = new TotalPurchaseAmount(name, quantity);
 
-        assertThat(totalPurchaseAmount.getPriceByName(name)).isEqualTo(1500);
+        assertThat(totalPurchaseAmount.findProductName(name).getPrice()).isEqualTo(1500);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "물, false",
+            "컵라면, true",
+            "오렌지주스, true"
+    })
+    void 상품이_프로모션_혜택을_받는지_확인(String input, boolean expectedResult) {
+        int quantity = 1;
+        TotalPurchaseAmount totalPurchaseAmount = new TotalPurchaseAmount(input, quantity);
+        Products name = totalPurchaseAmount.findProductName(input);
+
+        assertThat(totalPurchaseAmount.checkProductsThatBenefit(name)).isEqualTo(expectedResult);
     }
 }
