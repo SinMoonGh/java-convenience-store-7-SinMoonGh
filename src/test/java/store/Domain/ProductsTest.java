edffd5ba8a861057d54bef23b7f9e954f7f7a1b5
SinMoonGh <109773691+SinMoonGh@users.noreply.github.com>
2024-11-07 20:20:12 +0900

    test(ProductsTest): 프로모션 상품 호출 테스트 생성
    - 상품 데이터 중에서 프로모션 혜택이 적용된 상품만 호출합니다.

--- src/test/java/store/Domain/ProductsTest.java
+++ src/test/java/store/Domain/ProductsTest.java
@@ -2,6 +2,9 @@ package store.Domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 
 public class ProductsTest {
@@ -16,5 +19,17 @@ public class ProductsTest {
         assertEquals(price, 1500);
         assertEquals(quantity, 6);
         assertEquals(promotion, null);
+    }
+
+    @Test
+    void 프로모션_상품_호출() {
+        List<String> result = Arrays.asList("콜라", "사이다", "물", "비타민워터", "감자칩", "초코바", "에너지바", "정식도시락", "컵라면");
+
+        List<String> promotionProducts = Arrays.stream(Products.values())
+                .filter(product -> product.getPromotion() == null)
+                .map(Products::getName)
+                .collect(Collectors.toList());
+
+        assertEquals(promotionProducts, result);
     }
 }
