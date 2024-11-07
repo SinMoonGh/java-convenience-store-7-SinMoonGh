9c92cea036e525a3d6409204dbb69793b8474687
SinMoonGh <109773691+SinMoonGh@users.noreply.github.com>
2024-11-07 21:08:20 +0900

    feat(TotalPurchaseAmount): 상품 이름으로 가격 조회 기능 추가

--- src/main/java/store/Domain/TotalPurchaseAmount.java
+++ src/main/java/store/Domain/TotalPurchaseAmount.java
@@ -1,4 +1,28 @@
 package store.Domain;
 
 public class TotalPurchaseAmount {
+
+    private final String name;
+    private final int quantity;
+
+    public TotalPurchaseAmount(String name, int quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    public static int getPriceByName(String productName) {
+        for (Products product : Products.values()) {
+            if (product.getName().equals(productName)) {
+                return product.getPrice();
+            }
+        }
+        throw new IllegalArgumentException("해당 이름의 상품을 찾을 수 없습니다: " + productName);
+    }
+
+    public int calculate() {
+        int price = getPriceByName(name);
+        int sum = price * quantity;
+
+        return sum;
+    }
 }
