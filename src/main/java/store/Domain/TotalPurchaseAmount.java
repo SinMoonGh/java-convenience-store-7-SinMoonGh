a2bf4c3741db3d18f87e629442d0469681229106
SinMoonGh <109773691+SinMoonGh@users.noreply.github.com>
2024-11-07 23:19:59 +0900

    feat(TotalPurchaseAmount): 프로모션 할인 상품인지 확인하는 기능 추가
    - findProductName메서드에서 응답을 받아서 프로모션 값이 존재하는 지
      확인합니다.

--- src/main/java/store/Domain/TotalPurchaseAmount.java
+++ src/main/java/store/Domain/TotalPurchaseAmount.java
@@ -10,17 +10,21 @@ public class TotalPurchaseAmount {
         this.quantity = quantity;
     }
 
-    public static int getPriceByName(String productName) {
+    public Products findProductName(String productName) {
         for (Products product : Products.values()) {
             if (product.getName().equals(productName)) {
-                return product.getPrice();
+                return product;
             }
         }
         throw new IllegalArgumentException("해당 이름의 상품을 찾을 수 없습니다: " + productName);
     }
 
+    public boolean checkProductsThatBenefit(Products product) {
+        return product.getPromotion() != null;
+    }
+
     public int calculate() {
-        int price = getPriceByName(name);
+        int price = findProductName(name).getPrice();
         int sum = price * quantity;
 
         return sum;
