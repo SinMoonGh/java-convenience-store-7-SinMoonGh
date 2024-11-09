4f709515e5359b404a7564429a1b242c53785a68
SinMoonGh <109773691+SinMoonGh@users.noreply.github.com>
2024-11-09 13:59:48 +0900

    feat(PromotionDiscount): comparePurchaseQuantityAndPromotionBuy 메서드 추가
    - 이벤트 중인 상품이 있을 때 증정 상품을 받을 수 있는 갯수만큼 구매했는 지에 대한 결과를
      반환합니다.

--- src/main/java/store/Domain/PromotionDiscount.java
+++ src/main/java/store/Domain/PromotionDiscount.java
@@ -6,25 +6,30 @@ import store.Utils.Inquiry;
 
 public class PromotionDiscount {
 
-    //    private final int quantity;
+    private final int purchaseQuantity;
     private final LocalDate now;
     private final Inquiry inquiry;
     private final LocalDate promotionEndDate;
 
-    public PromotionDiscount(LocalDateTime now, Inquiry inquiry) {
+    public PromotionDiscount(int purchaseQuantity, LocalDateTime now, Inquiry inquiry) {
+        this.purchaseQuantity = purchaseQuantity;
         this.now = now.toLocalDate();
         this.inquiry = inquiry;
         this.promotionEndDate = inquiry.findPromotion().getEndDate();
     }
 
     public PromotionDiscount(LocalDate now, LocalDate promotionEndDate) {
+        this.purchaseQuantity = 0;
         this.now = now;
         this.inquiry = null;
         this.promotionEndDate = promotionEndDate;
     }
 
     public boolean checkTheEvent() {
-
         return !now.isAfter(promotionEndDate);
+    }
+
+    public boolean comparePurchaseQuantityAndPromotionBuy() {
+        return purchaseQuantity == inquiry.findPromotion().getBuy();
     }
 }
